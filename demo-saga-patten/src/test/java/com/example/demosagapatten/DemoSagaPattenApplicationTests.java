feeec9c049e2910f5112c7526a1fffb9e8926bfa
Nho Hoang <htnho97@gmail.com>
2022-07-29 13:16:28 +0700

    start project

---
+++ demo-saga-patten/src/test/java/com/example/demosagapatten/DemoSagaPattenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demosagapatten;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSagaPattenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
